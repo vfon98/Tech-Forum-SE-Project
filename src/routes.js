f5c0de88979b0ec2e54d6ed7008a2505aa020a1f
vfon98 <vfon98@gmail.com>
2020-02-24 10:58:55 +0700

    Initialize basic project structure

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+export const routes = [
+  {
+    path: '',
+    component: null,
+    exact: true,
+    render: null,
+    children: null
+  }
+]
---
+++ src/store/index.js
@@ -0,0 +1,7 @@
+import reducers from '../reducers';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+
+let store = createStore(reducers, applyMiddleware(thunk));
+
+export default store;
