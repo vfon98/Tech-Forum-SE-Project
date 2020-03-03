73409b6f33e4c9fc77065cd93850c8d105e3785f
Nguyễn Khoa <nguyenkhoa@Nguyen-Khoas-MacBook-Pro.local>
2020-03-03 13:05:15 +0700

    Add new page: Discussion Page

---
+++ client/src/assets/jss/discussionViewStyles.jsx
@@ -0,0 +1,15 @@
+import { container } from "./main";
+
+const discussionViewStyles = {
+  container: {
+    ...container
+  },
+  background: {
+    background: "#fff",
+    padding: '5rem 0'
+  }
+
+}
+
+console.log(discussionViewStyles.container)
+export default discussionViewStyles;
